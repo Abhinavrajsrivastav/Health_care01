fed02f14c770c7c7caa5325c66ed73755e9a40c7
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2023-12-26 19:48:29 +0530

    26-12-2023-001

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -4,6 +4,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/google", google);
-router.get('/signout', signOut)
+router.get('/signout', signOut);
 
 export default router;
